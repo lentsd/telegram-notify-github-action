a704416387f106f568144b3a48469c752e4de030
dnotrad <lentsd@bk.ru>
2022-10-30 12:02:37 +0300

    add pull detailed info

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import * as core from '@actions/core';
+const core = require('@actions/core');
+const github = require('@actions/github');
 const Bot = require('node-telegram-bot-api');
 
 const tgtoken = core.getInput('token', { required: true });
@@ -7,29 +8,55 @@ const prAuthor = core.getInput('prAuthor');
 const prNumber = core.getInput('prNumber');
 const prBody = core.getInput('prBody');
 const prTitle = core.getInput('prTitle');
-const repository = core.getInput('repository');
+const repositoryName = core.getInput('repositoryName');
+const repositoryOwner = core.getInput('repositoryOwner');
+const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
 
-const reviewersByPrAuthor = {
-    dnotrad: ['@plsdie', '@tim_kim_tim'],
-    overkam: ['@lentsd', '@tim_kim_tim'],
-    timkimtim: ['@lentsd', '@plsdie'],
-};
+const main = async () => {
 
-const PR_MESSAGE = `
+    const bot = new Bot(tgtoken);
+    const octokit = new github.getOctokit(GITHUB_TOKEN);
+
+    const reviewersByPrAuthor = {
+        dnotrad: ['@plsdie', '@tim_kim_tim'],
+        overkam: ['@lentsd', '@tim_kim_tim'],
+        timkimtim: ['@lentsd', '@plsdie'],
+    };
+
+    const { data: changedFiles } = await octokit.rest.pulls.listFiles({
+        owner: repositoryOwner,
+        repo: repositoryName,
+        pull_number: prNumber,
+    });
+
+    const { additions, deletions } = changedFiles.reduce((result, current) => {
+        result.additions += current.additions;
+        result.deletions += current.deletions;
+        return result;
+    }, { additions: 0, deletions: 0 })
+
+    const PR_MESSAGE = `
 Опа! Новый пр #${prNumber}!
 Барин ${prAuthor} радует своих подданых:
 
-*Что сделали*: ${prTitle}
+*🙋🏼Что сделали🙋🏼*: ${prTitle}
+
+*📏Файлов изменили📏*: ${changedFiles.length}
+🪜*Добавили*🪜: ${additions} строк
+🧹*Удалили*🧹: ${deletions} строк
+
 *Подробнее*: 
 ${prBody}
 
-[Посмотреть прчик](https://github.com/${repository}/pull/${prNumber})
+[Посмотреть прчик](https://github.com/${repositoryOwner}/${repositoryName}/pull/${prNumber})
 `
 
-const REVIEWERS = `
+    const REVIEWERS = `
 ${reviewersByPrAuthor[prAuthor].reduce((res, next) => res += `${next} `, '')}
 `
 
-const bot = new Bot(tgtoken);
-bot.sendMessage(chatid, PR_MESSAGE, { parse_mode: "Markdown" });
-bot.sendMessage(chatid, REVIEWERS);
+    bot.sendMessage(chatid, PR_MESSAGE, { parse_mode: "Markdown" });
+    bot.sendMessage(chatid, REVIEWERS);
+};
+
+main()
